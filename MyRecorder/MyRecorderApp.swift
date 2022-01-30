132c5407bd2aba609bf5387caf7cd80f7cdc87d4
Iurie Guzun <iurie_guzun@yahoo.com>
2022-01-30 13:37:58 -0500

    Initial Commit

---
+++ MyRecorder/MyRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRecorderApp.swift
+//  MyRecorder
+//
+//  Created by Iurie Guzun on 2022-01-30.
+//
+
+import SwiftUI
+
+@main
+struct MyRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
