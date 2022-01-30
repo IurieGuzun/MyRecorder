0f44edcea5da137421be4bad0b33be3de6f4c1bc
Iurie Guzun <iurie_guzun@yahoo.com>
2022-01-30 15:11:32 -0500

    Recorder designed

--- MyRecorder/AudioRecorder.swift
+++ MyRecorder/AudioRecorder.swift
@@ -11,5 +11,14 @@ import Combine
 import AVFoundation
 
 class AudioRecorder: ObservableObject {
+    
+    let objectWillChange = PassthroughSubject<AudioRecorder, Never>()
 
+    var audioRecorder: AVAudioRecorder!
+    
+    var recording = false {
+            didSet {
+                objectWillChange.send(self)
+            }
+        }
 }
--- MyRecorder/MyRecorderApp.swift
+++ MyRecorder/MyRecorderApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct MyRecorderApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(audioRecorder: AudioRecorder())
         }
     }
 }
