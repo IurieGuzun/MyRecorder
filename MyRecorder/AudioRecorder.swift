7d7786321e7a16d96acac96e9c163b0a4f1d3d3f
Iurie Guzun <iurie_guzun@yahoo.com>
2022-01-30 14:49:00 -0500

    Created AudioRecorder class

---
+++ MyRecorder/AudioRecorder.swift
@@ -0,0 +1,15 @@
+//
+//  AudioRecorder.swift
+//  MyRecorder
+//
+//  Created by Iurie Guzun on 2022-01-30.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import AVFoundation
+
+class AudioRecorder: ObservableObject {
+
+}
