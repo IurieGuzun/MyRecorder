c68fd35ebeb010730820a99e827e8c59cb66652b
Iurie Guzun <iurie_guzun@yahoo.com>
2022-01-30 16:23:38 -0500

    RecordingDataModel file created

---
+++ RecordingDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecordingDataModel.swift
+//  MyRecorder
+//
+//  Created by Iurie Guzun on 2022-01-30.
+//
+
+import Foundation
+
+struct Recording {
+    let fileURL: URL
+    let createdAt: Date
+}
